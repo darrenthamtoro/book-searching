d16ebd4a9a064c444e7286a76b6c40826ad12da8
darrenthamtoro <darrenthamtoro@gmail.com>
2021-09-03 14:36:24 -0700

    login and signup routes fixed

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,30 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
-      token
-      profile {
-        _id
-        name
-      }
-    }
-  }
-`;
-
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
       token
       user {
         _id
@@ -33,3 +11,51 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook( $book: String!) {
+    saveBook( book: $book) {
+      _id
+      username
+      savedBooks{
+        bookId,
+        title, 
+        description, 
+        authors,
+        link,
+        image
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(book: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId,
+        title, 
+        description, 
+        authors,
+        link,
+        image
+      }s
+    }
+  }
+`;
+
+
